99aa8514877d0738f36a423b0a66e6b1510de301
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-21 20:01:52 +0200

    Fix and add init back

--- CPP/cmake.mk
+++ CPP/cmake.mk
@@ -1,16 +1,16 @@
-.PHONY: cmake-update cmake-purge
+.PHONY: cmake-init cmake-update cmake-purge
 
 ROOT_DIRECTORY=external/dotfiles/
 CPP_DIRECTORY=CPP/
 MAKEFILE_DIRECTORY=make/
 CMAKE_CLIENT_MAKEFILE=cmake.mk
 
-# To instantiate the directory in git repository:
-# 	git submodule add --force --depth 1 https://github.com/sz3kz/dotfiles.git ${ROOT_DIRECTORY}
-# 	cd ${ROOT_DIRECTORY} && git sparse-checkout init --cone && git sparse-checkout set ${CPP_DIRECTORY}
+cmake-init:
+	git submodule add --force --depth 1 https://github.com/sz3kz/dotfiles.git ${ROOT_DIRECTORY}
+	cd ${ROOT_DIRECTORY} && git sparse-checkout init --cone && git sparse-checkout set ${CPP_DIRECTORY}
 
 cmake-update:
-	git submodule update --init
+	git submodule update --init --remote
 	cd ${ROOT_DIRECTORY}${CPP_DIRECTORY} && make deploy
 
 cmake-purge:
