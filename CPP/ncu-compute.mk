e5fee108ff67b9859e977e14ef97593a608173e2
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-25 01:05:50 +0200

    Fix ncu compute profiler client makefiles

--- CPP/ncu-compute.mk
+++ CPP/ncu-compute.mk
@@ -3,10 +3,10 @@
 SERVER_MAKEFILE_DIRECTORY=external/dotfiles/CPP/
 
 ncu-activate:
-	cd ${SERVER_MAKEFILE_DIRECTORY} && make dotfiles-activate
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make ncu-activate
 
 ncu-deactivate:
-	cd ${SERVER_MAKEFILE_DIRECTORY} && make dotfiles-deactivate
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make ncu-deactivate
 
 ncu-refresh:
-	cd ${SERVER_MAKEFILE_DIRECTORY} && make dotfiles-refresh
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make ncu-refresh
