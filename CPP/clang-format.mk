2c389e6b05929850debf5781ae5f3e9a45e9a864
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-25 14:22:28 +0200

    Give clang-format it's own makefile
    This way clang-format can be updated and ran through a makefile target.

---
+++ CPP/clang-format.mk
@@ -0,0 +1,14 @@
+.PHONY: clang-format-refresh clang-format-format
+FILE ?=
+
+SERVER_MAKEFILE_DIRECTORY=external/dotfiles/CPP/
+
+clang-format-refresh:
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make clang-format-refresh
+
+clang-format-format:
+	@if [ -z "$(FILE)" ]; then \
+		echo "Error: Please specify a file using 'make clang-format-format FILE=<FILENAME.CPP>"; \
+		exit 1; \
+	fi
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make clang-format-format CLANG_FORMAT_TARGET_FILE=${FILE}
