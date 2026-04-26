41945e98792b889463cc80f6a6fd91ec716c3c2a
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-26 12:28:15 +0200

    Delete clang-format-format
    The main idea behind the submodule is to deploy necessary files for
    various tools . It is not supposed to be a frontend for those tools.

--- CPP/clang-format.mk
+++ CPP/clang-format.mk
@@ -1,19 +1,10 @@
-.PHONY: clang-format-refresh clang-format-format clang-format-config-update
+.PHONY: clang-format-refresh clang-format-config-update
 .SILENT:
-
-FILE ?=
 
 SERVER_MAKEFILE_DIRECTORY=external/dotfiles/CPP/
 
 clang-format-refresh:
 	cd ${SERVER_MAKEFILE_DIRECTORY} && make clang-format-refresh
-
-clang-format-format:
-	@if [ -z "$(FILE)" ]; then \
-		echo "Error: Please specify a file using 'make clang-format-format FILE=<FILENAME.CPP>"; \
-		exit 1; \
-	fi
-	cd ${SERVER_MAKEFILE_DIRECTORY} && make clang-format-format CLANG_FORMAT_TARGET_FILE=${FILE}
 
 clang-format-config-update:
 	cd ${SERVER_MAKEFILE_DIRECTORY} && make clang-format-config-update
