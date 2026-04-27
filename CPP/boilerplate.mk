e59f324b30b802c6282c0922dec2e643b2b30d8e
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-27 21:57:15 +0200

    boilerplate-activate: Add dependency - boilerplate-check-prerequesites(client side)

--- CPP/boilerplate.mk
+++ CPP/boilerplate.mk
@@ -4,7 +4,7 @@
 REPOSITORY_ROOT_DIRECTORY=external/boilerplate/
 LANGUAGE_DIRECTORY=CPP/
 
-boilerplate-activate:
+boilerplate-activate: boilerplate-check-prerequesites
 	git submodule update --init
 	cd ${REPOSITORY_ROOT_DIRECTORY} && git sparse-checkout init --cone
 	cd ${REPOSITORY_ROOT_DIRECTORY} && git sparse-checkout set ${LANGUAGE_DIRECTORY}
@@ -19,4 +19,25 @@ boilerplate-refresh:
 	cd ${REPOSITORY_ROOT_DIRECTORY}${LANGUAGE_DIRECTORY} && make boilerplate-refresh
 
 boilerplate-check-prerequesites:
-	cd ${REPOSITORY_ROOT_DIRECTORY}${LANGUAGE_DIRECTORY} && make boilerplate-check-prerequesites
+	echo -n "Checking Prerequesite make:          "
+	command -v make >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite cmake:         "
+	command -v cmake >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite pre-commit:    "
+	command -v pre-commit >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite git:           "
+	command -v git >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite clang:         "
+	command -v clang >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite clang-format:  "
+	command -v clang-format >/dev/null
+	echo "Installed!"
+	echo -n "Checking Prerequesite clang-tidy:    "
+	command -v clang-tidy >/dev/null
+	echo "Installed!"
+	echo "Prerequesite Check Ended Successfully!"
