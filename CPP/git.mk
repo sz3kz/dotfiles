a43bae4fd5ae4e1df4c676a5ec13413a297d9306
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-30 18:06:07 +0200

    Pre-commit Hook: Move updating command from clang to git family

--- CPP/git.mk
+++ CPP/git.mk
@@ -1,4 +1,4 @@
-.PHONY: git-refresh git-update-ignore git-rehook
+.PHONY: git-refresh git-update-ignore git-rehook git-update-hook
 .SILENT:
 
 SERVER_MAKEFILE_DIRECTORY=external/boilerplate/CPP/
@@ -8,6 +8,9 @@ git-refresh:
 
 git-update-ignore:
 	cd ${SERVER_MAKEFILE_DIRECTORY} && make git-update-ignore
+
+git-update-hook:
+	cd ${SERVER_MAKEFILE_DIRECTORY} && make git-update-hook
 
 git-rehook:
 	cd ${SERVER_MAKEFILE_DIRECTORY} && make git-rehook
